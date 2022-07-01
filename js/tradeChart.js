1e81212836eb182b9e2109ad1a4e6ace605e6041
charliedu2000 <68285614+charliedu2000@users.noreply.github.com>
2022-07-01 08:42:05 +0800

    Remove unnecessary listener in init().

--- js/tradeChart.js
+++ js/tradeChart.js
@@ -4,6 +4,7 @@ tradeTypeTabs.addEventListener("click", (evt) => {
     const selected = evt.target.dataset.value;
     const tabs = document.getElementsByClassName("time-item");
     for (let index = 0; index < tabs.length; index++) {
+        // 为所有 tab 去除 active，发生事件的 tab 设为 active
         const tab = tabs[index];
         tab.classList.remove("active");
         if (index == selected) {
--- js/tradeType.js
+++ js/tradeType.js
@@ -1,6 +1,7 @@
 // 交易类型点击切换
 const freTableTabs = document.getElementsByClassName("trade-type")[0];
 freTableTabs.addEventListener("click", (evt) => {
+    // 按钮和输入框组都是一对，切换都用 toggle 即可
     document.getElementsByClassName("trade-traditional")[0]
         .classList.toggle("hidden");
     document.getElementsByClassName("trade-nlp")[0]
